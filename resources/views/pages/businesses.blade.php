4e298d5d923b7b524e57edd67f840fb6588d0e2f
indracodev <indracodev@gmail.com>
2026-05-04 09:09:17 +0700

    feat: create businesses page with export, packaging, distribution, and online store sections

--- resources/views/pages/businesses.blade.php
+++ resources/views/pages/businesses.blade.php
@@ -4,19 +4,19 @@
 
 @section('content')
 <main id="konten">
-    <h1 class="visually-hidden">{{ __('business_h1') }}</h1>
+    <h1 class="visually-hidden" data-i18n="business_h1">{{ __('business_h1') }}</h1>
     <div class="container">
         <!-- Section: Ekspor -->
         <section class="py-5">
             <div class="py-lg-5">
-                <h2 class="display-4 fw-thin text-capitalize">{!! __('ekspor_title') !!}</h2>
-                <p class="lead mb-5">{{ __('ekspor_desc') }}</p>
+                <h2 class="display-4 fw-thin text-capitalize" data-i18n="ekspor_title">{!! __('ekspor_title') !!}</h2>
+                <p class="lead mb-5" data-i18n="ekspor_desc">{{ __('ekspor_desc') }}</p>
                 <div class="d-flex gap-3 align-items-center">
                     <img src="{{ asset('images/exim2.png') }}" data-light="{{ asset('images/exim2.png') }}" data-dark="{{ asset('images/exim2-invert.png') }}" alt="" aria-hidden="true" loading="lazy" class="theme-image icon-rounded" style="aspect-ratio: 1/1; object-fit: contain;">
                     <ul class="mb-0">
-                        <li>{{ __('ekspor_list1') }}</li>
-                        <li>{{ __('ekspor_list2') }}</li>
-                        <li>{{ __('ekspor_list3') }}</li>
+                        <li data-i18n="ekspor_list1">{{ __('ekspor_list1') }}</li>
+                        <li data-i18n="ekspor_list2">{{ __('ekspor_list2') }}</li>
+                        <li data-i18n="ekspor_list3">{{ __('ekspor_list3') }}</li>
                     </ul>
                 </div>
             </div>
@@ -27,14 +27,14 @@
         <!-- Section: Packaging -->
         <section class="py-5">
             <div class="py-lg-5">
-                <h2 class="display-4 fw-thin text-capitalize">{!! __('packaging_title') !!}</h2>
-                <p class="lead mb-5">{{ __('packaging_desc') }}</p>
+                <h2 class="display-4 fw-thin text-capitalize" data-i18n="packaging_title">{!! __('packaging_title') !!}</h2>
+                <p class="lead mb-5" data-i18n="packaging_desc">{{ __('packaging_desc') }}</p>
                 <div class="d-flex gap-3 align-items-center">
                     <img src="{{ asset('images/layanan-pengemasan.png') }}" data-light="{{ asset('images/layanan-pengemasan.png') }}" data-dark="{{ asset('images/layanan-pengemasan-invert.png') }}" alt="" aria-hidden="true" loading="lazy" class="theme-image icon-rounded" style="aspect-ratio: 1/1; object-fit: contain;">
                     <ul class="mb-0">
-                        <li>{{ __('packaging_list1') }}</li>
-                        <li>{{ __('packaging_list2') }}</li>
-                        <li>{{ __('packaging_list3') }}</li>
+                        <li data-i18n="packaging_list1">{{ __('packaging_list1') }}</li>
+                        <li data-i18n="packaging_list2">{{ __('packaging_list2') }}</li>
+                        <li data-i18n="packaging_list3">{{ __('packaging_list3') }}</li>
                     </ul>
                 </div>
             </div>
@@ -45,21 +45,21 @@
         <!-- Section: Distribusi -->
         <section class="py-5">
             <div class="py-lg-5">
-                <h2 class="display-4 fw-thin text-capitalize">{!! __('business_dist_title') !!}</h2>
-                <p class="lead mb-5">{{ __('business_dist_desc') }}</p>
+                <h2 class="display-4 fw-thin text-capitalize" data-i18n="business_dist_title">{!! __('business_dist_title') !!}</h2>
+                <p class="lead mb-5" data-i18n="business_dist_desc">{{ __('business_dist_desc') }}</p>
                 <ul class="list-unstyled mb-0 row row-gap-5 gx-lg-5">
                     <li class="col col-12 col-lg-6 pe-xl-5">
                         <article>
                             <img src="{{ asset('images/businesses-domestic.png') }}" data-light="{{ asset('images/businesses-domestic.png') }}" data-dark="{{ asset('images/businesses-domestic-invert.png') }}" alt="" aria-hidden="true" loading="lazy" class="theme-image" style="aspect-ratio: 1/1; width: 20%; object-fit: contain;">
-                            <h3 class="fw-bold fs-4 text-capitalize my-3">{{ __('business_domestic_title') }}</h3>
-                            <p>{{ __('business_domestic_desc') }}</p>
+                            <h3 class="fw-bold fs-4 text-capitalize my-3" data-i18n="business_domestic_title">{{ __('business_domestic_title') }}</h3>
+                            <p data-i18n="business_domestic_desc">{{ __('business_domestic_desc') }}</p>
                         </article>
                     </li>
                     <li class="col col-12 col-lg-6 pe-xl-5 ps-xl-5">
                         <article>
                             <img src="{{ asset('images/businesses-global.png') }}" data-light="{{ asset('images/businesses-global.png') }}" data-dark="{{ asset('images/businesses-global-invert.png') }}" alt="" aria-hidden="true" loading="lazy" class="theme-image" style="aspect-ratio: 1/1; width: 20%; object-fit: contain;">
-                            <h3 class="fw-bold fs-4 text-capitalize my-3">{{ __('business_international_title') }}</h3>
-                            <p>{{ __('business_international_desc') }}</p>
+                            <h3 class="fw-bold fs-4 text-capitalize my-3" data-i18n="business_international_title">{{ __('business_international_title') }}</h3>
+                            <p data-i18n="business_international_desc">{{ __('business_international_desc') }}</p>
                         </article>
                     </li>
                 </ul>
@@ -71,14 +71,14 @@
         <!-- Section: Online Store -->
         <section class="py-5">
             <div class="py-lg-5">
-                <h2 class="display-4 fw-thin text-capitalize">{!! __('business_online_title') !!}</h2>
-                <p class="lead mb-5">{{ __('business_online_desc') }}</p>
+                <h2 class="display-4 fw-thin text-capitalize" data-i18n="business_online_title">{!! __('business_online_title') !!}</h2>
+                <p class="lead mb-5" data-i18n="business_online_desc">{{ __('business_online_desc') }}</p>
                 <ul class="list-unstyled mb-0 row row-gap-5 gx-lg-5">
                     <li class="col col-12 col-lg-6 pe-xl-5">
                         <article>
                             <img src="{{ asset('images/businesses-ecommerce.png') }}" data-light="{{ asset('images/businesses-ecommerce.png') }}" data-dark="{{ asset('images/businesses-ecommerce-invert.png') }}" alt="" aria-hidden="true" loading="lazy" class="theme-image" style="aspect-ratio: 1/1; width: 20%; object-fit: contain;">
-                            <h3 class="fw-bold fs-4 text-capitalize my-3">{{ __('business_ecommerce_title') }}</h3>
-                            <p class="mb-4">{{ __('business_ecommerce_desc') }}</p>
+                            <h3 class="fw-bold fs-4 text-capitalize my-3" data-i18n="business_ecommerce_title">{{ __('business_ecommerce_title') }}</h3>
+                            <p class="mb-4" data-i18n="business_ecommerce_desc">{{ __('business_ecommerce_desc') }}</p>
                             <ul class="list-unstyled row row-cols-2 row-gap-3 gx-5">
                                 <li class="col">
                                     <a href="https://supresso.com/" target="_blank" class="text-reset text-decoration-none">
@@ -96,8 +96,8 @@
                     <li class="col col-12 col-lg-6 pe-xl-5 ps-xl-5">
                         <article>
                             <img src="{{ asset('images/businesses-marketplace.png') }}" data-light="{{ asset('images/businesses-marketplace.png') }}" data-dark="{{ asset('images/businesses-marketplace-invert.png') }}" alt="" aria-hidden="true" loading="lazy" class="theme-image" style="aspect-ratio: 1/1; width: 20%; object-fit: contain;">
-                            <h3 class="fw-bold fs-4 text-capitalize my-3">{{ __('business_marketplace_title') }}</h3>
-                            <p class="mb-4">{{ __('business_marketplace_desc') }}</p>
+                            <h3 class="fw-bold fs-4 text-capitalize my-3" data-i18n="business_marketplace_title">{{ __('business_marketplace_title') }}</h3>
+                            <p class="mb-4" data-i18n="business_marketplace_desc">{{ __('business_marketplace_desc') }}</p>
                             <ul class="list-unstyled row row-cols-2 row-cols-sm-3 row-gap-3 gx-5 align-items-center">
                                 <li class="col">
                                     <a href="https://www.tokopedia.com/indracoofficial" target="_blank" class="text-reset text-decoration-none">
