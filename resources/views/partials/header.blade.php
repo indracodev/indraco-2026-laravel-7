51f1da911cffb002356b594a32d78668b2d669e4
indracodev <indracodev@gmail.com>
2026-05-08 09:00:30 +0700

    Update translations and header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -238,9 +238,9 @@
                <li><a href="{{ route('contact') }}" class="text-reset text-decoration-none opacity-75-hover {{ request()->routeIs('contact') ? 'fw-bold text-primary' : '' }}" data-i18n="nav_contact">{{ __('nav_contact') }}</a></li>
              </ul>
              <div class="d-flex column-gap-4">
-                <button type="button" class="btn p-0 rounded-0 border-0" data-bs-toggle="modal" data-bs-target="#modal-search">
-                   <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 640 640"><path fill="currentColor" d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" /></svg>
-                </button>
+                <!--<button type="button" class="btn p-0 rounded-0 border-0" data-bs-toggle="modal" data-bs-target="#modal-search">-->
+                <!--   <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 640 640"><path fill="currentColor" d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" /></svg>-->
+                <!--</button>-->
                 <button type="button" class="theme-toggle">
                    <svg aria-hidden="true" class="theme-icon" width="1.25em" height="1.25em" viewBox="0 0 24 24" fill="none">
                       <g class="icon-sun">
